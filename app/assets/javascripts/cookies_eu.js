f9065060d50bc3a9f859e0122f39ed55235f3402
Antonio Borrero Granell <antonio@bridge-u.com>
2015-01-07 13:43:28 +0000

    Fix bug

--- app/assets/javascripts/cookies_eu.js
+++ app/assets/javascripts/cookies_eu.js
@@ -2,7 +2,7 @@
 //= require jquery.cookie
 
 $(document).ready(function() {
-  if !$.cookie('cookie_eu_consented') {
+  if (!$.cookie('cookie_eu_consented')) {
     $('a').click(function(e) {
       $.cookie('cookie_eu_consented', 'true', { path: '/'});
       $('.cookies-eu').remove();
