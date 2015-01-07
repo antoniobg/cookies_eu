1c4e1dd3e6b919937ad76ad88eb2d703a7675b73
Antonio Borrero Granell <antonio@bridge-u.com>
2015-01-07 13:24:41 +0000

    Remove OK button. Accepts cookie policy by continuing using the website

--- app/assets/javascripts/cookies_eu.js
+++ app/assets/javascripts/cookies_eu.js
@@ -2,8 +2,8 @@
 //= require jquery.mobile
 //= require jquery.cookie
 
-$(document).ready( function(){
-  $('.cookies-eu-ok').click(function(e){
+$(document).ready(function() {
+  $('a').click(function(e) {
     e.preventDefault();
     $.cookie('cookie_eu_consented', 'true', { path: '/'});
     $('.cookies-eu').remove();
